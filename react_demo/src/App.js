1849eae5b6a61566f9c7f73d0e3e5ea2a9238b45
shangwenbin <swb@meitu.com>
2018-11-26 10:34:51 +0800

    react demo

--- react_demo/src/App.js
+++ react_demo/src/App.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import FunnelChart from './components/FunnelChart';
+import { funnelChartData } from './mock/mock';
+
+let mockData = funnelChartData;
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
+        <FunnelChart data={mockData} percentStep={2} fixedType="floor" baseNum={undefined} noDataText="no data"></FunnelChart>
           <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
---
+++ react_demo/src/mock/mock.js
@@ -0,0 +1,27 @@
+// 漏斗图mock数据
+const funnelChartData = [{
+    pv: 2121,
+    action: 'PV'
+}, {
+    pv: 1822,
+    action: 'UV'
+}, {
+    pv: 1434,
+    action: 'Click'
+}, {
+    pv: 1266,
+    action: 'Show'
+}, {
+    pv: 1067,
+    action: 'See'
+}, {
+    pv: 733,
+    action: 'Order'
+}, {
+    pv: 500,
+    action: 'Success'
+}];
+
+export {
+    funnelChartData
+};
