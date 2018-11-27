680d1df37a85498204525b28f2c1faa34aa06bb6
fe_bean <wind7killer@163.com>
2018-11-28 00:41:01 +0800

    dev

--- vue_demo/src/views/Home.vue
+++ vue_demo/src/views/Home.vue
@@ -2,16 +2,21 @@
 <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <LineChart></LineChart>
+    <DrawPage></DrawPage>
 </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
-
+import LineChart from '@/components/LineChart.vue';
+import DrawPage from '@/components/DrawPage.vue';
 @Component({
     components: {
-        HelloWorld
+        HelloWorld,
+        LineChart,
+        DrawPage
     }
 })
 export default class Home extends Vue {}
