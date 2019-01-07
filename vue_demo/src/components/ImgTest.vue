4d611cf7c78a7b3e633d1ba697c5210650f98430
fe_bean <wind7killer@163.com>
2019-01-08 00:03:26 +0800

    dev

--- vue_demo/src/components/ImgTest.vue
+++ vue_demo/src/components/ImgTest.vue
@@ -21,6 +21,9 @@ export default {
     },
     mounted() {
         this.audioDom = this.$refs.audio;
+        audioDom.oncanplay = () => {
+            audioDom.play();
+        }
     },
     computed: {
         getStage() {
