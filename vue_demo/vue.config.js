dc29b3f2fba5fed5be7e19edb1268800e0ea29cf
fe_bean <wind7killer@163.com>
2019-01-07 23:34:45 +0800

    dev

--- vue_demo/vue.config.js
+++ vue_demo/vue.config.js
@@ -1,5 +1,13 @@
 module.exports = {
     configureWebpack: {
         devtool: 'source-map'
+    },
+    baseUrl: '//win7killer.github.io/demo_set/vue_demo/dist/',
+    indexPath: '../index.html',
+    devServer: {
+        open: process.platform === 'darwin',
+        host: '0.0.0.0',
+        disableHostCheck: true,
+        port: 80,
     }
 };
